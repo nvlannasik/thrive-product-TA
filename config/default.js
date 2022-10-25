a604fcead8d40c13c416c11ab600571943738c3c
nvlannasik <annasikn@yahoo.com>
2022-10-25 23:42:06 +0700

    add config

---
+++ config/default.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const confidence = require("confidence");
+
+const config = {
+  $meta: "This file configures the plot device.",
+  projectName: "thrive-ecommerce",
+  rabbitmq: {
+    host: process.env.RABBITMQ_HOST,
+    port: process.env.RABBITMQ_PORT,
+    username: process.env.RABBITMQ_USERNAME,
+    password: process.env.RABBITMQ_PASSWORD,
+    exchange: process.env.RABBITMQ_EXCHANGE,
+    routingKey: process.env.RABBITMQ_ROUTINGKEY,
+    queue: process.env.RABBITMQ_QUEUE,
+    vhost: process.env.RABBITMQ_VHOST,
+    silent: true,
+  },
+  server: {
+    port: process.env.PORT_SERVER,
+  },
+};
+
+const store = new confidence.Store(config);
+
+exports.get = function (key) {
+  return store.get(key);
+};
