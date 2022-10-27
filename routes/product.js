f4784f22b500f541c93a6686c6c0c069398fbab5
nvlannasik <annasikn@yahoo.com>
2022-10-28 03:05:17 +0700

    add product api update

--- routes/product.js
+++ routes/product.js
@@ -84,4 +84,42 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//UPDATE STOCK PRODUCT
+
+router.patch("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404).send({
+      status: "error",
+      message: "Product not found",
+    });
+    logger.error(
+      `[${req.method}] - 404 - ${res.statusMessage} - ${req.originalUrl} - ${req.ip}`
+    );
+  }
+  try {
+    const updatedProduct = await Product.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          quantity: req.body.quantity,
+        },
+      }
+    );
+    res.status(200).send({
+      status: "success",
+      message: "Product updated successfully",
+      data: updatedProduct,
+    });
+    logger.info(
+      `[${req.method}] - 200 - ${res.statusMessage} - ${req.originalUrl} - ${req.ip}`
+    );
+  } catch (err) {
+    res.status(400).send(err);
+    logger.error(
+      `[${req.method}] - 400 - ${res.statusMessage} - ${req.originalUrl} - ${req.ip}`
+    );
+  }
+});
+
 module.exports = router;
